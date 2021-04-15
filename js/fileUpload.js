9c45d2a8d8393a69b4873c9f3de2ed97082fd7a9
Franklin Yu <franklinyu777@gmail.com>
2021-04-15 12:04:00 -0400

    Updated file upload.js to report error message when trying to upload an image without selecting one

--- js/fileUpload.js
+++ js/fileUpload.js
@@ -2,22 +2,25 @@ function uploadFile(fileNumber) {
     var form = new FormData();
     var picture = document.querySelector('#productImage'+fileNumber).files[0];
     
-    form.append('file', picture);
+	if(picture!=null){
+		form.append('file', picture);
 
-    var upload = new XMLHttpRequest();
-    upload.open('POST', 'upload.php');
-    upload.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status == 200) {
-            if(this.responseText == 1) {
-				document.querySelector('#productImage'+fileNumber+'UpdatedMessage').innerText = "Image uploaded successfully.";        
-				document.querySelector('#productImage'+fileNumber+'Error').innerText = '';
-                document.querySelector('#productImage'+fileNumber+'ErrorInput').value = '';
-            } else {
-                document.querySelector('#productImage'+fileNumber+'Error').innerText = this.responseText;
-                document.querySelector('#productImage'+fileNumber+'ErrorInput').value = this.responseText;
-				document.querySelector('#productImage'+fileNumber+'UpdatedMessage').innerText='';
-            }
-        }
-    }
-    upload.send(form);
+		var upload = new XMLHttpRequest();
+		upload.open('POST', 'upload.php');
+		upload.onreadystatechange = function() {
+			if(this.readyState == 4 && this.status == 200) {
+				if(this.responseText == 1) {
+					document.querySelector('#productImage'+fileNumber+'UpdatedMessage').innerText = "Image uploaded successfully.";        
+					document.querySelector('#productImage'+fileNumber+'Error').innerText = '';
+					document.querySelector('#productImage'+fileNumber+'ErrorInput').value = '';
+				} else {
+					document.querySelector('#productImage'+fileNumber+'Error').innerText = this.responseText;
+					document.querySelector('#productImage'+fileNumber+'ErrorInput').value = this.responseText;
+					document.querySelector('#productImage'+fileNumber+'UpdatedMessage').innerText='';
+				}
+			}
+		}
+		upload.send(form);
+	}
+	else document.querySelector('#productImage'+fileNumber+'Error').innerText = 'Please select a picture';
 }
